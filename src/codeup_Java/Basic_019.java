c9d4205555af80c5a442c2158336ced7f472e324
NegotiationMan <dwc07238@naver.com>
2022-12-26 23:01:49 +0900

    기초-입출력

--- src/codeup_Java/Basic_019.java
+++ src/codeup_Java/Basic_019.java
@@ -7,7 +7,7 @@ public class Basic_019 {
 //        String str = sc.nextLine();
 //        String[] index = str.split("\\.");                   // 정규식에서의 .은 임의의 문자열을 의미
 //        String[] index = str.split("[.]");                   // 이렇게 사용해도 ok
-        String[] index = sc.nextLine().split("\\.");    // 이렇게 한번에 코드 작성 가능
+        String[] index = sc.nextLine().split("[.]");    // 이렇게 한번에 코드 작성 가능
 
         int y = Integer.parseInt(index[0]);
         int m = Integer.parseInt(index[1]);
